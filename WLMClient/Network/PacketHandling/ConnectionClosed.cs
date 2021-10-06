2e62841ae702623afe395f042d6f6961d3fbf6e6
zeltr <zeltr@LAPTOP-E3QKTV96>
2021-10-06 17:37:04 +0200

    Ajoutez des fichiers projet.

---
+++ WLMClient/Network/PacketHandling/ConnectionClosed.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+
+using WLMClient.UI.Windows;
+using WLMData.Enums;
+using WLMData.Data.Packets;
+using WLMClient.Locale;
+
+namespace WLMClient.Network.PacketHandling
+{
+    class ConnectionClosed : PacketHandler
+    {
+        public ConnectionClosed(MainWindow mainWindow) : base(mainWindow) { }
+
+        private NetworkComms.ConnectionEstablishShutdownDelegate closeHandler;
+
+        public override void InitializePacket()
+        {
+            closeHandler = new NetworkComms.ConnectionEstablishShutdownDelegate((Connection connection) =>
+            {
+                mainWindow.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal, (Action)(() =>
+                {
+                    mainWindow.ConnectionClosedLogOut();
+                }));
+            });
+
+            Open();
+        }
+
+        public void Open()
+        {
+            NetworkComms.AppendGlobalConnectionCloseHandler(closeHandler);
+        }
+
+        public void Close()
+        {
+            NetworkComms.RemoveGlobalConnectionCloseHandler(closeHandler);
+        }
+    }
+}
